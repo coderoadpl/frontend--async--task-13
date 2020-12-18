00a0d794d1921f6d0c9cd092ad2634d118673fba
Mateusz Choma <kontakt@coderoad.pl>
2020-12-18 16:36:28 +0100

    Debounc simplest implementation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!')
+const sayHello = () => {
+    console.log('Hello!')
+}
+
+const TIME = 1000 // ms
+
+let timeoutId
+
+for (let i = 0; i < 100; i++) {
+    if(timeoutId) clearTimeout(timeoutId)
+
+    timeoutId = setTimeout(
+        sayHello,
+        TIME
+    )
+}
